a4b1a7e04c2aa6c0602744c65abfe6a899491960
Mik-C <miquelclark@hotmail.com>
2017-05-12 11:32:34 +0200

    constants.h

---
+++ src/constants.h
@@ -0,0 +1,10 @@
+//
+// Created by Miquel Floriach Clark on 12/5/17.
+//
+
+#ifndef TJE_FRAMEWORK_2017_CONSTANTS_H
+#define TJE_FRAMEWORK_2017_CONSTANTS_H
+
+typedef unsigned int UID;
+
+#endif //TJE_FRAMEWORK_2017_CONSTANTS_H
